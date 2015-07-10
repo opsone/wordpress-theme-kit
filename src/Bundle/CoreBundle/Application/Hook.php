ad6c01f6343bbab59dbe0c716eb65a01197b65e2
Jeremy <jeremy@opsone.net>
2015-07-10 17:41:07 +0200

    Restriction admin for user no admin

--- src/Bundle/CoreBundle/Application/Hook.php
+++ src/Bundle/CoreBundle/Application/Hook.php
@@ -142,4 +142,15 @@ class Hook
 
         return $template;
     }
+
+    public static function restrictAccessAdministration()
+    {
+        global $pagenow;
+        if($pagenow == 'admin-ajax.php') return;
+
+        if (current_user_can('subscriber') && !current_user_can('administrator')) {
+          wp_redirect(get_bloginfo('url'));
+          exit();
+        }
+    }
 }
