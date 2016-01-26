f6f6c1b2940ac5949247374b053801af4bc1e26a
Jeremy <jeremy@opsone.net>
2016-01-26 13:09:03 +0100

    Remove redirect automatique

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -31,7 +31,8 @@ add_filter('wp_mail_content_type'       , array('Bundle\CoreBundle\Application\F
 add_filter('sanitize_file_name'         , 'remove_accents');
 
 
-remove_action('do_feed_rdf' , 'do_feed_rdf' , 10, 1);
-remove_action('do_feed_rss' , 'do_feed_rss' , 10, 1);
-remove_action('do_feed_rss2', 'do_feed_rss2', 10, 1);
-remove_action('do_feed_atom', 'do_feed_atom', 10, 1);
+remove_action('do_feed_rdf'      , 'do_feed_rdf' , 10, 1);
+remove_action('do_feed_rss'      , 'do_feed_rss' , 10, 1);
+remove_action('do_feed_rss2'     , 'do_feed_rss2', 10, 1);
+remove_action('do_feed_atom'     , 'do_feed_atom', 10, 1);
+remove_action('template_redirect', 'redirect_canonical');
