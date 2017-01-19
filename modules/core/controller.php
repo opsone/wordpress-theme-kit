93695627bf3010f5cbdab8cf3adca511e4ec154b
Jeremy <jeremy@opsone.net>
2017-01-19 15:18:11 +0100

    Move controller in modules

--- modules/core/controller.php
+++ modules/core/controller.php
@@ -1,22 +1,27 @@
 <?php
 
+function initController($controller_name)
+{
+  $controller = new $controller_name();
+  $vars = $controller->init();
+
+  foreach ($vars as $key => $value) {
+    $GLOBALS[$key] = $value;
+  }
+}
+
 function loadController($template)
 {
-    $template_name   = ucfirst(str_replace('.php', '', basename($template)));
-    $template_name   = ucwords(str_replace('-', ' ', $template_name));
-    $template_name   = str_replace(' ', '', $template_name);
-    $controller_name = 'Controller\\'.$template_name.'Controller';
+  $template_name   = ucfirst(str_replace('.php', '', basename($template)));
+  $template_name   = ucwords(str_replace('-', ' ', $template_name));
+  $template_name   = str_replace(' ', '', $template_name);
+  $controller_name = 'Controller\\'.$template_name.'Controller';
 
-    if(class_exists($controller_name)) {
-        $controller = new $controller_name();
-        $vars = $controller->init();
+  if(class_exists($controller_name)) {
+    initController($controller_name);
+  }
 
-        foreach ($vars as $key => $value) {
-          $GLOBALS[$key] = $value;
-        }
-    }
-
-    return $template;
+  return $template;
 }
 
 add_action('template_include', 'loadController');
