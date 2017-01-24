4a95dd4cae4655e0aabcf58e517961378ae2c532
Jeremy <jeremy@opsone.net>
2017-01-24 11:30:06 +0100

    Remove gulp and add webpack

--- modules/assets/css.php
+++ modules/assets/css.php
@@ -2,16 +2,9 @@
 
 function loadCss()
 {
-    wp_register_style('style', get_template_directory_uri() . '/assets/front/dist/styles.min.css', null, false);
+    wp_register_style('style', get_template_directory_uri() . '/dist/style.css', null, false);
 
-    wp_enqueue_style('style');
-}
-
-function adminLoadCss()
-{
-    wp_register_style('style', get_template_directory_uri() . '/assets/admin/dist/styles.min.css', null, false);
     wp_enqueue_style('style');
 }
 
 add_action('wp_enqueue_scripts', 'loadCss');
-add_action('admin_print_scripts', 'adminLoadCss');
