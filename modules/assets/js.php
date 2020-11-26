9c97786c5ec7745991a76e61bc1166888e4430d0
Jeremy <jeremy@opsone.net>
2020-11-26 17:12:16 +0100

    Fix assets

--- modules/assets/js.php
+++ modules/assets/js.php
@@ -7,13 +7,15 @@ function loadJs()
 
   wp_register_script('app', asset_url("wp-content/themes/$name/assets/build/front.js"), array(), $theme->get('Version'), true);
   wp_register_script('runtime', asset_url("wp-content/themes/$name/assets/build/runtime.js"), array(), $theme->get('Version'), true);
-  wp_register_script('vendors', asset_url("wp-content/themes/$name/assets/build/vendors~admin~front.js"), array(), $theme->get('Version'), true);
+  wp_register_script('vendors', asset_url("wp-content/themes/$name/assets/build/vendors~front.js"), array(), $theme->get('Version'), true);
+  wp_register_script('vendors2', asset_url("wp-content/themes/$name/assets/build/vendors~admin~front.js"), array(), $theme->get('Version'), true);
 
   wp_localize_script('app', 'ajaxurl', admin_url('admin-ajax.php'));
 
   wp_enqueue_script('app');
   wp_enqueue_script('runtime');
   wp_enqueue_script('vendors');
+  wp_enqueue_script('vendors2');
 }
 
 function adminLoadJs()
